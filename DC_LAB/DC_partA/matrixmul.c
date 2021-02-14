e279d7a647d443b80125dadb07b991fdcecfe6e1
Umer-Faruk <umer.gvt@gmail.com>
2021-02-15 00:37:30 +0530

    update

--- DC_LAB/DC_partA/matrixmul.c
+++ DC_LAB/DC_partA/matrixmul.c
@@ -1,35 +1,27 @@
-#include<omp.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<omp.h>
+
 
 void main(){
 
-     int r=5,c=5,vects=5,i,j;
-     int M[r][c],vect[vects],resvect[vects];
+     int M[2][2]={{1,4},{4,7}};
+     int V[2]={1,1};
+     int RV[2];
 
-     for(i=0;i<r;i++){
-          for(j=0;j<c;j++){
-               M[i][j] = i+j;
+     #pragma omp parallel for private(V)
+     for(int i = 0;i<2;i++){
+          for(int j=0;j<2;j++){
                printf("%d \t",M[i][j]);
+               RV[i] = RV[i]+M[i][j]*V[j];
           }
           printf("\n");
+          
      }
 
-     for(i =0;i<vects;i++){
-          vect[i]=i;
-     }
-     // omp_set_num_threads(32);
-     #pragma omp parallel for 
-     for(i=0;i<r;i++){
-          for(j=0;j<c;j++){
-               resvect[i] =  resvect[i] + M[i][j] * vect[j];
-          }
+     for(int i=0;i<2;i++){
+          printf("%d\n",RV[i]);
      }
 
-
-     printf("result\n");
-     for(i=0;i<r;i++){
-          printf("%d \n",resvect[i]);
-     }
-
+      
 }
